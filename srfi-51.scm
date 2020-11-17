58cc0f4f150d4a81a3e769b57acadd5be0ad08a2
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-16 22:58:07 -0800

    Fixed missing exports

--- srfi-51.scm
+++ srfi-51.scm
@@ -3,7 +3,19 @@
   (import (chicken base))
   (import (chicken module))
   (import (chicken platform))
+  (import (chicken module))
   (import srfi-1)
+
+  (export
+   rest-values
+   arg-and
+   arg-ands
+   err-and
+   err-ands
+   arg-or
+   arg-ors
+   err-or
+   err-ors)
 
   (register-feature! 'srfi-51)
 
